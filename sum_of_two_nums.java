59c6a2bb6e0e68602414578debf986e31204d215
Rks2005 <131243597+Rks2005@users.noreply.github.com>
2026-06-19 21:03:22 +0530

    Sum of 2 nums

---
+++ sum_of_two_nums.java
@@ -0,0 +1,8 @@
+public class sum_of_two_nums {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
