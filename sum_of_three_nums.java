23f2a104540f1a810f8a0d48efc5bfaf06b513e5
Rks2005 <131243597+Rks2005@users.noreply.github.com>
2026-06-19 21:04:02 +0530

    Sum of 3 nums

---
+++ sum_of_three_nums.java
@@ -0,0 +1,9 @@
+class sum_of_three_nums {
+    public static void main(String []args){
+        int num1=10;
+        int num2=20;
+        int num3=30;
+        int sum=num1+num2+num3;
+        System.out.println("The sum of "+num1+" and "+num2+" and "+num3+" is: "+sum);
+    }
+}
